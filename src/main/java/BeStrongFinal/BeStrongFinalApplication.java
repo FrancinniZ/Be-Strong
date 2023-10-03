7435d5f267b350d38944e642ad2eeb8bebbddbe5
Francinni ZC <113738269+FrancinniZ@users.noreply.github.com>
2023-10-02 20:42:21 -0600

    FINALIZADO SIN IMAGENES Y VIDEOS

---
+++ src/main/java/BeStrongFinal/BeStrongFinalApplication.java
@@ -0,0 +1,13 @@
+package BeStrongFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeStrongFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeStrongFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/BeStrongFinal/BeStrongFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package BeStrongFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeStrongFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
